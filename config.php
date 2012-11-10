288e62db32afc53b5a73decadc87a84df771051b
adaminmelbourne <me@adamchant.com>
2012-11-10 16:15:10 +1100

    removed install and created sample config file

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.harrywragg.dev:8888/');
+define('HTTP_IMAGE', 'http://shop.harrywragg.dev:8888/image/');
+define('HTTP_ADMIN', 'http://shop.harrywragg.dev:8888/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.harrywragg.dev:8888/');
+define('HTTPS_IMAGE', 'http://shop.harrywragg.dev:8888/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/catalog/');
+define('DIR_SYSTEM', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/system/');
+define('DIR_DATABASE', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/system/database/');
+define('DIR_LANGUAGE', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/catalog/language/');
+define('DIR_TEMPLATE', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/catalog/view/theme/');
+define('DIR_CONFIG', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/system/config/');
+define('DIR_IMAGE', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/image/');
+define('DIR_CACHE', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/download/');
+define('DIR_LOGS', '/Volumes/Data/Adam/Documents/projects/shop.harrywragg.dev/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'shop.harrywragg.dev');
+define('DB_PREFIX', '');
+?>
